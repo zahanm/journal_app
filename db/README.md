2cdb73232922e2f0a083adfe7496a5d8f832b3b1
Zahan Malkani <zahanm@gmail.com>
2011-10-05 23:38:35 -0700

    empty new django app, settings modifications

---
+++ db/README.md
@@ -0,0 +1,5 @@
+
+### Database files live here
+
+- *.sqlite3 for now
+- don't delete unless you want ot reset the db and run migrations again
--- settings.py
+++ settings.py
@@ -4,15 +4,15 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+  ('Zahan Malkani', 'zahanm@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    'NAME': '',                      # Or path to database file if using sqlite3.
+    'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+    'NAME': 'db/development.sqlite3', # Or path to database file if using sqlite3.
     'USER': '',                      # Not used with sqlite3.
     'PASSWORD': '',                  # Not used with sqlite3.
     'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -27,7 +27,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -103,20 +103,20 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'ppjournal.urls'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+  # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+  # Always use forward slashes, even on Windows.
+  # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
-  'django.contrib.auth',
+  # 'django.contrib.auth',
   'django.contrib.contenttypes',
   'django.contrib.sessions',
   'django.contrib.sites',
-  'django.contrib.messages',
+  # 'django.contrib.messages',
   'django.contrib.staticfiles',
   # Uncomment the next line to enable the admin:
-  # 'django.contrib.admin',
+  'django.contrib.admin',
   # Uncomment the next line to enable admin documentation:
   # 'django.contrib.admindocs',
 )
