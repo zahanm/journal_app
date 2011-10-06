701b7ef1a461b6861778f1e80ca521a7c7dccd80
Zahan Malkani <zahanm@gmail.com>
2011-10-06 00:26:56 -0700

    Added some models
    Journal and Section

--- journalweb/models.py
+++ journalweb/models.py
@@ -1,3 +1,17 @@
+
 from django.db import models
 
-# Create your models here.
+class Journal(models.Model):
+  name = models.CharField(max_length=100)
+  pdf = models.FileField()
+
+TYPES_OF_SECTIONS = (
+  ('text', 'text transcription'),
+  ('math', 'equations and LaTeX')
+)
+
+class Section(models.Model):
+  auto_id = models.AutoField()
+  img = models.ImageField()
+  trans_type = models.CharField(max_length=4, choices=TYPES_OF_SECTIONS)
+  transcription = models.TextField(default='')
