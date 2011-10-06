e63b2c5780d14906403d189bfc1f78f9e22b23a6
Zahan Malkani <zahanm@gmail.com>
2011-10-06 00:47:41 -0700

    Activate journalweb and model tweaks
    mostly file-related stuff

--- journalweb/models.py
+++ journalweb/models.py
@@ -3,7 +3,9 @@ from django.db import models
 
 class Journal(models.Model):
   name = models.CharField(max_length=100)
-  pdf = models.FileField()
+  pdf = models.FileField(upload_to='journals')
+  def __unicode__(self):
+    return self.name
 
 TYPES_OF_SECTIONS = (
   ('text', 'text transcription'),
@@ -11,7 +13,9 @@ TYPES_OF_SECTIONS = (
 )
 
 class Section(models.Model):
-  auto_id = models.AutoField()
-  img = models.ImageField()
+  auto_id = models.AutoField(primary_key=True)
+  img = models.ImageField(upload_to='sections')
   trans_type = models.CharField(max_length=4, choices=TYPES_OF_SECTIONS)
   transcription = models.TextField(default='')
+  def __unicode__(self):
+    return self.trans_type + ': ' + self.img.url
