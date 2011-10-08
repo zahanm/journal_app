c0dbe7b2a20adc64da66a13502e52d858d3b2716
Zahan Malkani <zahanm@gmail.com>
2011-10-08 03:11:54 -0700

    Reorganized urlconf

--- urls.py
+++ urls.py
@@ -4,11 +4,15 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-  # url(r'^ppjournal/', include('ppjournal.foo.urls')),
-  url(r'^$', 'transcribe.views.index', name='home'),
-  url(r'^journal/add/$', 'journal.views.add'),
-  url(r'^journal/(?P<journal_id>\d+)/$', 'journal.views.show'),
+urlpatterns = patterns('transcribe.views',
+  url(r'^$', 'index', name='home'),
+)
 
+urlpatterns += patterns('journal.views',
+  url(r'^journal/add/$', 'add'),
+  url(r'^journal/(?P<journal_id>\d+)/$', 'show'),
+)
+
+urlpatterns += patterns('',
   url(r'^admin/', include(admin.site.urls)),
 )
