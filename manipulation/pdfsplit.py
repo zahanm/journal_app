6500432cec59084fa38819268f1af2704276a79b
Zahan Malkani <zahanm@gmail.com>
2011-10-29 16:56:23 -0700

    UIST hack removal, keep required directories

--- manipulation/pdfsplit.py
+++ manipulation/pdfsplit.py
@@ -49,36 +49,11 @@ def divide_page(page_num, png_fname):
     upper += segment_height
     lower += segment_height
 
-# --- UIST HACK START
-
-SPLIT_HEIGHTS = [
-  [138, 180, 214, 292, 323, 355, 374, 450, 500, 560, 630, 683, 731],
-  [180, 244, 275, 343, 361, 400, 424, 460, 481, 565, 598, 734],
-  [133, 153, 170, 227, 257, 286, 313, 405, 425, 493, 603, 621, 726],
-  [105, 168, 244, 295, 368, 720],
-  [122, 155, 241, 277, 308, 480, 510, 540, 592, 616, 723],
-  [190, 224, 450, 479, 717]
-]
-
-def divide_page_manual(page_num, png_fname):
-  page = Image.open(png_fname)
-  page_width, page_height = page.size
-  left, upper, right = 0, 0, page_width
-  for segment_num, lower in enumerate(SPLIT_HEIGHTS[page_num]):
-    segment_fname = 'data/segment_{0}_{1}.png'.format(page_num, segment_num)
-    segment = page.copy()
-    segment = segment.crop((left, upper, right, lower))
-    segment.save(segment_fname)
-    yield segment_fname
-    upper = lower
-
-# --- UIST HACK END
-
 def split_pdf(pdf_fname):
   output = []
   pdf_fnames = split_pages(pdf_fname)
   for page, png_fname in enumerate(convert_pages(pdf_fnames)):
-    for segment_fname in divide_page_manual(page, png_fname): # divide_page(page, png_fname)
+    for segment_fname in divide_page(page, png_fname):
       output.append({ 'location': segment_fname, 'page': page })
   with open(DOG_INPUT_FNAME, 'w') as dog_input:
     json.dump(output, dog_input)
@@ -86,10 +61,8 @@ def split_pdf(pdf_fname):
 
 def cleanup_last_run():
   file_globs = ['data/*.png', 'tmp/*.pdf', 'tmp/*.png', 'tmp/*.tex',
-    'tmp/*.aux', 'tmp/*.log', 'output/*.pdf', 'output/images/*.png']
-  old_files = []
-  map(lambda fg: old_files.extend(glob(fg)), file_globs)
-  map(lambda old_file: os.remove(old_file), old_files)
+    'tmp/*.aux', 'tmp/*.log', 'output/*.pdf']
+  map(lambda fg: os.remove(glob(fg)), file_globs)
 
 if __name__ == '__main__':
   if(len(sys.argv) == 2):
