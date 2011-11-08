4e4a51a829076dde2a4098e141fbc13f882ab3d8
Zahan Malkani <zahanm@gmail.com>
2011-11-07 16:06:41 -0800

    use render() shortcut, binds the RequestContext

--- journal/views.py
+++ journal/views.py
@@ -1,18 +1,19 @@
 
-from django.shortcuts import render_to_response, get_object_or_404, redirect
-from django.template import RequestContext
+from django.shortcuts import render, get_object_or_404, redirect
 
 from journal.forms import JournalForm
+from manipulation.pdfsplit import split_pdf
 
 def add(request):
   if request.method == 'POST':
     form = JournalForm(request.POST, request.FILES)
     if form.is_valid():
       form.save()
+      split_pdf()
       return redirect('home')
   else:
     form = JournalForm()
-  return render_to_response('add.html', { 'form': form }, context_instance=RequestContext(request))
+  return render(request, 'add.html', { 'form': form })
 
 def show(request, journal_id):
-  return render_to_response('view.html', { 'id': journal_id })
+  return render(request, 'view.html', { 'id': journal_id })
