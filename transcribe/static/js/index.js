bd71bdba2e73f7528b800f4b1171eb659b7f3b81
Zahan Malkani <zahanm@gmail.com>
2011-11-10 19:02:08 -0800

    use dormousejs approrpiately in test js

--- transcribe/static/js/index.js
+++ transcribe/static/js/index.js
@@ -1,7 +1,9 @@
 
 (function() {
-  $dm.server = 'http://arya.stanford.edu:3777/';
-  $dm.api_key = '';
-  console.log('Home page');
+  $dm.server('http://arya.stanford.edu:3777/');
+  $dm.api_key('6b044f121358683678e5e21de2202a5e0a0394d5');
   console.log($dm);
+  $dm.getTasks(function(r) {
+    console.log(r);
+  });
 })();
